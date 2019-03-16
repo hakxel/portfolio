3f7ffbd4c4967e82109d6cd48b57f2adf033ff32
Hakxel <asiel_montes@yahoo.com>
2019-03-16 11:28:02 -0400

    Remove logo and toggle from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,12 @@ let Navbar = () => {
   return (
     <header id="header">
       <nav class="nav-wrapper">
-        <div class="nav-logo"><a href="/">
-          <img id="logo-img" src={logo} alt="Asiel" /></a>
-        </div>
         <div class="nav-items">
           <ul class="nav nav-pills">
             <li class="active"><a href="#top">Home</a></li>
             <li class=""><a href="#profile">Projects</a></li>
             <li class=""><a href="#bio">Bio</a></li>
           </ul>
-        </div>
-        <div class="nav-checkbox">
-          <p>Music?</p>
-          <label class="switch">
-            <input type="checkbox"/>
-              <span class="slider round"></span>
-          </label>
         </div>
       </nav>         
     </header>

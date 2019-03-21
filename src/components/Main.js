2b9e2b4ba887351d77faa3dae741dce1fa3b6f35
Hakxel <asiel_montes@yahoo.com>
2019-03-21 08:58:08 -0400

    Modify position of hero element

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,12 +9,12 @@ let Main = () => {
     <section id="hero">
       <Navbar />
       <div class="hero-body">
-        <SocialLinks />
-        <div class="hero-wrapper">
+        <SocialLinks />        
+        <div class="hero-right">
           <h1>Asiel Montes</h1>
           <h2>Web developer</h2>
+          <a href="#contact">Contact me</a>
         </div>
-        {/* <div class="hero-right"></div> */}
       </div>
     </section>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,11 @@ let Navbar = () => {
   return (
     <header id="header">
       <nav className="nav-wrapper">
-        <div className="nav-items">
-          <ul className="nav nav-pills">
-            <li className="active"><a href="#top">Home</a></li>
-            <li className=""><a href="#bio">Bio</a></li>
-            <li className=""><a href="#projects">Projects</a></li>
-          </ul>
-        </div>
+        <ul className="nav-pills">
+          <li className="active"><a href="#top">Home</a></li>
+          <li className=""><a href="#bio">Bio</a></li>
+          <li className=""><a href="#projects">Projects</a></li>
+        </ul>
       </nav>         
     </header>
   )

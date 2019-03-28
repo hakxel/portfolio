7c56028c6a9b974dcd88c77fcbf3c2a3f13d7408
Hakxel <asiel_montes@yahoo.com>
2019-03-28 12:30:57 -0400

    Shorten linkedin profile link

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -7,7 +7,7 @@ let SocialLinks = () => {
   return (
     <aside className="hero-left">
       <div className="icon-wrapper">
-        <a href="https://www.linkedin.com/in/asiel-montes-27b95612a/" target="_blank" rel="noopener noreferrer"><img className="linkedin icon" src={linkedin} alt="Linkedin icon" /></a>
+        <a href="https://www.linkedin.com/in/asiel-montes/" target="_blank" rel="noopener noreferrer"><img className="linkedin icon" src={linkedin} alt="Linkedin icon" /></a>
       </div>
       <div className="icon-wrapper">
         <a href="https://github.com/Hakxel" target="_blank" rel="noopener noreferrer"><img className="github icon" src={github} alt="Github icon" /></a>

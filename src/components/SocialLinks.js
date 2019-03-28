a6f6332193c9d0fcb463b5b9739b6a11f1e393fb
Hakxel <asiel_montes@yahoo.com>
2019-03-28 12:10:42 -0400

    Finish initial hover styles

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -6,9 +6,15 @@ import github from '../images/icons-github.svg'
 let SocialLinks = () => {
   return (
     <aside className="hero-left">
-      <a href="https://www.linkedin.com/in/asiel-montes-27b95612a/" target="_blank" rel="noopener noreferrer"><img id="linkedin-icon" src={linkedin} alt="Linkedin icon" /></a>
-      <a href="https://github.com/Hakxel" target="_blank" rel="noopener noreferrer"><img id="github-icon" src={github} alt="Github icon" /></a>
-      <a href="https://twitter.com/Hakxell" target="_blank" rel="noopener noreferrer"><img id="twitter-icon" src={twitter} alt="Twitter icon" /></a>
+      <div className="icon-wrapper">
+        <a href="https://www.linkedin.com/in/asiel-montes-27b95612a/" target="_blank" rel="noopener noreferrer"><img className="linkedin icon" src={linkedin} alt="Linkedin icon" /></a>
+      </div>
+      <div className="icon-wrapper">
+        <a href="https://github.com/Hakxel" target="_blank" rel="noopener noreferrer"><img className="github icon" src={github} alt="Github icon" /></a>
+      </div>
+      <div className="icon-wrapper">
+        <a href="https://twitter.com/Hakxell" target="_blank" rel="noopener noreferrer"><img className="twitter icon" src={twitter} alt="Twitter icon" /></a>
+      </div>
     </aside>
   )
 }

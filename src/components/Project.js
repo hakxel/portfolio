b2efa14f53e4d147d8bce9b13431019abaed437f
Hakxel <asiel_montes@yahoo.com>
2019-03-28 15:54:56 -0400

    Finish project image hover functionality

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ class Project extends Component {
     return(
       <div className={this.props.class}>
         <a href={this.props.href} target="_blank" rel="noopener noreferrer">
-          <img src={process.env.PUBLIC_URL + this.props.image} alt={this.props.alt}></img>
+          <img src={process.env.PUBLIC_URL + this.props.image} name={this.props.name} onMouseEnter={this.props.onHoverIn} onMouseLeave={this.props.onHoverOut} alt={this.props.alt}></img>
           <p>{this.props.description}</p>
         </a>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ProjectsList from '../data/projects.json'
 import Project from './Project';
 
-// import SampleApp from '../images/SampleApp.png'
-// import LECSS from '../images/LECSS.png'
-// import PalindromeApp from '../images/palindrome_app.png'
-// import JacqueFresco from '../images/tribute_jacque_fresco.png'
-// import ExpenseTracker from '../images/expense_tracker_app.png'
-// import BullsAndCows from '../images/bulls_and_cows.png'
-
-const imgSource = process.env.PUBLIC_URL
-
 const projectList = ProjectsList.allProjects
-let Projects = () => {
-  console.log(imgSource)
-  return(
-    <section id="projects">
-      <div className="title">
-        <h3>Projects</h3>
-      </div>
-      <div className="projects-wrapper">
-        {projectList.map( project =>
-          <Project 
-            key={project.name}
-            class={project.class}
-            href={project.href}
-            image={project.image}
-            alt={project.alt}
-            description={project.description}
-          />
-        )}
-      </div>
-    </section>
-  )
-}
+class Projects extends Component {
+  constructor(){
+    super()
+    this.state = {
+      bitworldBuys: "1.png",
+      beatBlender: "1.png",
+      sampleApp: "1.png",
+      leCSS: "1.png"
+    }
+  }
+
+  handleMouseIn = (event) => {
+    let name = event.target.name
+    this.setState({
+      [name]: "2.png"
+    })
+  }
+
+  handleMouseOut = (event) => {
+    let name = event.target.name
+    this.setState({
+      [name]: "1.png"
+    })
+  }
+
+  render(){
+      
+      return(
+        <section id="projects">
+          <div className="title">
+            <h3>Projects</h3>
+          </div>
+          <div className="projects-wrapper">
+            {projectList.map( (project, index) =>
+              <Project 
+                key={index}
+                name={project.name}
+                class={project.class}
+                href={project.href}
+                image={project.image + this.state[project.name]}
+                onHoverIn={this.handleMouseIn}
+                onHoverOut={this.handleMouseOut}
+                alt={project.alt}
+                description={project.description}
+              />
+            )}
+          </div>
+        </section>
+      )
+    }
+  }
 
 export default Projects

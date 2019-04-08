962e38409e500e981378865afa89767f6647c70c
Hakxel <asiel_montes@yahoo.com>
2019-04-08 15:46:58 -0400

    Remove toggle css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './main.css'
-import './toggle.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
